be7126f675aedf048aab1c408aa807e86d024b59
Datnguyen <datndd2604@gmail.com>
2022-04-11 22:35:47 +0700

    redux in flutter update

--- lib/redux/actions.dart
+++ lib/redux/actions.dart
@@ -1,13 +1,7 @@
-// Actions: Actions are payloads of information that send data from your
-// application to your store. They are the only source of information
-// for the store. You send them to the store using store.dispatch().
-
-// TODO 3: Create a UpdateDrinkAction with a payload "updatedDrink"
-
 import 'package:smtutorial/models/drink.dart';
 
 class UpdateDrinkAction {
   final Drink updatedDrink;
 
-  UpdateDrinkAction(Drink drink, {this.updatedDrink});
+  UpdateDrinkAction(this.updatedDrink);
 }
--- lib/redux/redux_screen.dart
+++ lib/redux/redux_screen.dart
@@ -21,56 +21,52 @@ class ReduxScreen extends StatelessWidget {
           child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: Container(
-                padding: EdgeInsets.all(8.0),
-                decoration: kWhiteBackground,
-                // TODO 10: Surround the column with a StoreConnector
-                // TODO 10.1: convert the state in a List of Drinks
-
-                child: StoreConnector<AppState, List<Drink>>(
-                  converter: (store) => store.state.drinks,
-                  builder: (context, List<Drink> allDrinks) => Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Drinks tonight",
-                        style: Theme.of(context).textTheme.headline4,
-                      ),
-                      ...allDrinks
-                          .map(
-                            (drink) => DrinksWidget(
-                              drink: drink,
-                              onChanged: (value) {
-                                // TODO 11: Use the StoreProvider.of to dispatch the UpdateDrinkAction
-                                StoreProvider.of<AppState>(context).dispatch(
-                                  UpdateDrinkAction(drink),
-                                );
-                              },
-                            ),
-                          )
-                          .toList(),
-                      Text(
-                        "The order is: ",
-                        style: Theme.of(context).textTheme.headline4,
-                      ),
-                      Expanded(
-                        child: ListView.builder(
-                          itemBuilder: (context, index) => ListTile(
-                            title: Text(
-                              // TODO 12: replace with the state drinks and remove the drinks list from the widget
-                              allDrinks
-                                  .where((element) => element.selected)
-                                  .toList()[index]
-                                  .name,
-                            ),
+              padding: EdgeInsets.all(8.0),
+              decoration: kWhiteBackground,
+              child: StoreConnector<AppState, List<Drink>>(
+                converter: (store) => store.state.drinks,
+                builder: (context, List<Drink> allDrinks) => Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Drinks tonight",
+                      style: Theme.of(context).textTheme.headline4,
+                    ),
+                    ...allDrinks
+                        .map(
+                          (drink) => DrinksWidget(
+                            drink: drink,
+                            onChanged: (value) {
+                              StoreProvider.of<AppState>(context).dispatch(
+                                UpdateDrinkAction(drink),
+                              );
+                            },
                           ),
-                          itemCount: allDrinks
-                              .where((element) => element.selected)
-                              .length,
+                        )
+                        .toList(),
+                    Text(
+                      "The order is: ",
+                      style: Theme.of(context).textTheme.headline4,
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                        itemBuilder: (context, index) => ListTile(
+                          title: Text(
+                            allDrinks
+                                .where((element) => element.selected)
+                                .toList()[index]
+                                .name,
+                          ),
                         ),
+                        itemCount: allDrinks
+                            .where((element) => element.selected)
+                            .length,
                       ),
-                    ],
-                  ),
-                )),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
         ),
       ),
